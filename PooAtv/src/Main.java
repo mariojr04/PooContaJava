de2fabb724e5359e82521357ceb12a2d86525493
mariojr04 <162380419+mariojr04@users.noreply.github.com>
2025-03-11 10:26:05 -0300

    Update Main.java
    acrescentei Sout

--- PooAtv/src/Main.java
+++ PooAtv/src/Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String[] args){
         Conta c1 = new Conta("61864245379",14 ,354.00,true );
         System.out.println(c1.Depositar(143.00));
-        c1.Sacar(15.00);
-        c1.Bonificacao();
+        System.out.println(c1.Sacar(15.00));
+        System.out.println(c1.Bonificacao());
     }
 }
